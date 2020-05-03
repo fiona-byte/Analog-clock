a296eb2396b43b2650534a8e3c3d47ec4d40be40
unknown <fionaodoboh@gmail.com>
2020-05-03 23:14:30 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector(".c");
+const minHand = document.querySelector(".b");
+const hourHand = document.querySelector(".a");
+
+function setDate() {
+   const now = new Date();
+   const seconds = now.getSeconds();
+   const secondsDegrees = ((seconds / 60) * 360) + 90;
+   secondHand.style.transform ="rotate" + "(" + secondsDegrees +"deg" + ")";
+
+   const minutes = now.getMinutes();
+   const minsDegrees = ((minutes / 60) * 360) + 90;
+   minHand.style.transform ="rotate" + "(" + minsDegrees +"deg" + ")";
+   
+   const hours = now.getHours();
+   const hourDegrees = ((hours / 12) * 360) + 90;
+   hourHand.style.transform ="rotate" + "(" + hourDegrees +"deg" + ")";
+};
+
+
+setInterval(() => {
+    setDate ()
+}, 1000);
